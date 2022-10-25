f8b6cd6e299576ba6b7e5a9d26305579b8c7eb13
Nikolay Antonovich <TesaloNick@gmail.com>
2022-10-25 20:16:12 +0300

    add logos, standing

--- src/Pages/PageStanding/PageStanding.jsx
+++ src/Pages/PageStanding/PageStanding.jsx
@@ -5,22 +5,68 @@ import axios from 'axios';
 export default function PageStanding() {
   const seasons = [2021, 2020, 2019, 2018, 2017, 2016, 2015]
   const [dataStandingTable, setdataStandingTable] = useState([])
+  const teamIDs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
   useEffect(() => {
-    const options = {
-      method: 'GET',
-      url: 'https://www.balldontlie.io/api/v1/games?team_ids[]=14&per_page=100&seasons[]=2021',
-    };
+    // const options = {
+    //   method: 'GET',
+    //   url: 'https://www.balldontlie.io/api/v1/games?seasons[]=2021&team_ids[]=15&per_page=100&postseason=false',
+    // };
 
-    axios.request(options)
-      .then(res => {
-        let data = res.data.data
-        data = data.sort((a, b) => a.id - b.id)
-        return setdataStandingTable(res.data.data)
-      })
-      .catch((error) => console.error(error));
+    // axios.request(options)
+    //   .then(res => {
+    //     let data = res.data.data
+    //     data = data.sort((a, b) => a.id - b.id)
+    //     return setdataStandingTable(res.data.data)
+    //   })
+    //   .catch((error) => console.error(error));
+
+    let teamsInfo = []
+    teamIDs.map(async teamID => {
+      axios.get(`https://www.balldontlie.io/api/v1/games?seasons[]=2021&team_ids[]=${teamID}&per_page=100&postseason=false&start_date=2021-10-01`)
+        .then(res => {
+          let wins = 0
+          let logo = ''
+          res.data.data.map(item => {
+            if (item.status === "Final") {
+              if (item.home_team.id === teamID) {
+                logo = item.home_team.abbreviation
+                if (item.home_team_score > item.visitor_team_score) wins++
+              } else {
+                logo = item.visitor_team.abbreviation
+                if (item.visitor_team_score > item.home_team_score) wins++
+              }
+            }
+          })
+          teamsInfo = [...teamsInfo, {
+            wins,
+            losses: 82 - wins,
+            logo: `/teams-logo-images/${logo}-2023.png`
+          }]
+          setdataStandingTable(teamsInfo)
+        })
+    })
   }, [])
   console.log(dataStandingTable);
+
+  async function getInfo() {
+    const id = 15
+    return await axios.get(`https://www.balldontlie.io/api/v1/games?seasons[]=2021&team_ids[]=${id}&per_page=100&postseason=false&start_date=2021-10-01`)
+      .then(res => {
+        let wins = 0
+        res.data.data.map(item => {
+          if (item.status === "Final") {
+            if (item.home_team.id === id) {
+              if (item.home_team_score > item.visitor_team_score) wins++
+            } else {
+              if (item.visitor_team_score > item.home_team_score) wins++
+            }
+          }
+        })
+        console.log(wins);
+        return wins
+      })
+  }
 
   return (
     <div className={style.standing}>
@@ -31,13 +77,11 @@ export default function PageStanding() {
       </form>
 
       <div className={style.table}>
-        {dataStandingTable.map((row, index) => (
-          // <div key={index} className={style.table__row}>
+        {dataStandingTable.map((team, index) => (
           <div key={index}>
-            {/* <div className={style.table__logo}><img className={style.table__logo_img} src={row.team.logo} alt="" /></div> */}
-            <div className={style.table__name}>{row.date}</div>
-            {/* <div className={style.table__win}>{row.win.total}</div>
-            <div className={style.table__loss}>{row.loss.total}</div> */}
+            <div className={style.table__logo}><img className={style.table__logo_img} src={team.logo} alt="" /></div>
+            <div className={style.table__name}>{team.wins}</div>
+            <div className={style.table__win}>{team.losses}</div>
           </div>
         ))}
       </div>
